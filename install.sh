e17c12095c0ea881637527dfec80567c293cacf1
psygreg <psygreg@icloud.com>
2026-09-16 01:03:50 -0300

    fix install.sh for updater

--- install.sh
+++ install.sh
@@ -285,7 +285,10 @@ installer() {
 	error "Unsupported operating system."
 }
 
-if [ -t 0 ]; then
+if [ "${LINUXTOYS_NONINTERACTIVE:-0}" = "1" ]; then
+    header
+    installer
+elif [ -t 0 ]; then
 	header
 	printf 'Do you wish to install or update LinuxToys? (y/n): '
 	read -r _answer < /dev/tty
